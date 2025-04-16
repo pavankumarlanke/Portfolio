2595e6cbf6aa7cdf7f6a70c9f5937e38bfa5f5fe
Pavan Kumar Lanke <109908347+pavankumarlanke@users.noreply.github.com>
2025-04-16 11:12:16 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,6 @@ const CustomNavbar = () => {
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#skills">Skills</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
