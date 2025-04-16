f554e9c7914f379b6f1e541eddb86213afc4c44c
Pavan Kumar Lanke <109908347+pavankumarlanke@users.noreply.github.com>
2025-04-16 10:29:53 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const CustomNavbar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="portfolio-navbar">
+      <Container>
+        <Navbar.Brand href="#home" className="portfolio-logo">
+          <FontAwesomeIcon icon={faCode} className="mr-2" />
+          Pavan
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
